24bcd8cac8ba4db291c82d1fe3acf95a3d886dae
kar156505@gmail.com <kar156505@gmail.com>
2024-10-21 23:45:12 +0530

    changes in frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,23 @@
+import { useEffect , useState } from 'react'
 const Home = () => {
+    const [workouts , setWorkouts] = useState(null)
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workout/')
+            const json = response.json()
+
+            if(response.ok){
+                setWorkouts(json)
+            }
+        }
+        fetchWorkouts()
+    },[])
     return(
         <div className="home">
-            <h2>Home</h2>
+            <h1>Hello</h1>
+            <h2>{workouts && workouts.map((workout)=>(
+                <p key={workout._id}>{workout.title}</p>
+            ))}</h2>
         </div>
     )
 }
